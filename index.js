e7d9ee2fa2fc0d4af9f219ea3fb992b17936cbb7
Sito8943 <sito8943@gmail.com>
2023-06-16 07:40:36 -0700

    1.6.4 fixed parsing where

--- index.js
+++ index.js
@@ -56,13 +56,17 @@ const prepareWhere = (where) => {
           const { attribute, operator, value, value1, logic } = cond;
           switch (operator) {
             case "BETWEEN":
-              return `${i !== 0 ? logic : ""} BETWEEN ${value} AND ${value1}`;
+              string += `${
+                i !== 0 ? logic : ""
+              } BETWEEN ${value} AND ${value1}`;
+              break;
             case "IN":
-              return `${
+              string += `${
                 i !== 0 ? logic : ""
               } ${attribute} IN (${value.toString()})`;
+              break;
             default:
-              string = `${i !== 0 ? logic : ""} ${attribute} ${operator} ${
+              string += `${i !== 0 ? logic : ""} ${attribute} ${operator} ${
                 typeof value === "string" ? `'${value}'` : value
               }`;
               break;
